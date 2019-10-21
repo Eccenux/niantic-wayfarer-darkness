1e8b7df82a727e25d1f4cf36b9e23a2a19595bfd
eccenux <eccenux+git@gmail.com>
2019-10-21 04:03:50 +0200

    grid layout(s) for review boxes

--- niantic-wayfarer-darkness.user.js
+++ niantic-wayfarer-darkness.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OPR dark look and feel
 // @namespace    pl.enux.opr
-// @version      0.2.2
+// @version      0.3.0
 // @description  Dark skin for OPR aka Niantic Wayfarer (portal reviews)
 // @author       Eccenux
 // @match        https://wayfarer.nianticlabs.com/*
@@ -18,13 +18,148 @@
 	document.querySelector('.niantic-loader').parentNode.className=""
 */
 
-/**
-	Add CSS.
-*/
-function addCss() {
+//
+// Review grids media queries
+//
+var gridMedia = /*css*/ `
+@media screen and (min-width: 1720px) {
+	.card-area .card-row-container {
+		grid-template-columns: repeat(5, auto);
+		grid-template-areas:
+		"photo desc support minis what"
+		"dup   dup  map     map   comments"
+		;
+	}
+}
+@media screen and (min-width: 1340px) and (max-width: 1719px) {
+	.card-area .card-row-container {
+		grid-template-columns: repeat(4, auto);
+		grid-template-areas:
+			"photo desc support what"
+			"minis minis minis  comments"
+			"dup   dup  map     _"
+		;
 
-	// base CSS
-	var cssText = /*css*/ `
+	}
+	.three-card-parent {
+		width: 1120px;
+		grid-template-columns: repeat(3, auto);
+	}
+	#map-card {
+		width: 320px;
+	}	  
+}
+@media screen and (min-width: 0px) and (max-width: 1339px) {
+	.card-area .card-row-container {
+		grid-template-columns: calc(50vw - 30px) calc(50vw - 30px);
+		grid-template-areas:
+			"photo support"
+			"desc  minis"
+			"dup   map"
+			"what  comments"
+		;
+	}
+	.card--double-width, .card-style--double-width {
+		width: 100%;
+	}
+}
+@media screen and (min-width: 1025px) and (max-width: 1339px) {
+	.card, .card-style {
+		width: 100% !important;
+	}
+	.three-card-parent {
+		width: 100%;
+	}
+	.card-area .card-row-container {
+		grid-template-columns: calc(50vw - 30px - 90px) calc(50vw - 30px - 90px);
+	}
+}
+`;
+
+//
+// Review grids layout
+//
+var reviewLayout = /*css*/ `
+////
+// main review grid
+.card-area .card-row-container {
+  display: grid;
+  width: max-content;
+  max-width: unset;
+  overflow-y: auto;
+  grid-gap: 15px;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-areas:
+   "photo desc support minis what"
+   "dup   dup  map     map   comments"
+  ;
+}
+
+#three-card-container {
+  grid-area: minis;
+}
+#map-card {
+  grid-area: map;
+}
+#duplicates-card {
+  grid-area: dup;
+}
+#photo-card {
+  grid-area: photo;
+}
+#descriptionDiv {
+  grid-area: desc;
+}
+#supporting-card {
+  grid-area: support;
+}
+#what-is-it-card {
+  grid-area: what;
+}
+#additional-comments-card {
+  grid-area: comments;
+}
+
+////
+// extra for grid
+
+// scroll
+.card-area {
+	overflow-y: auto;
+}  
+
+// maps
+.card--double-width, .card-style--double-width {
+    width: 740px;
+}
+
+// reset margin
+.card-area .card-row-container .card {
+	margin: 0 !important;
+}
+
+// three-card (small cards)
+.three-card-parent {
+	height: auto;
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-gap: 15px;
+	margin: 0 !important;
+}
+.small-card .card-header__description {
+	font-size: 90%;
+}
+.small-card .card__header {
+	margin-bottom: 0;
+}
+
+${gridMedia}
+`
+
+//
+// base CSS
+//
+var baseCss = /*css*/ `
 :root {
 	--happy-headers-color: #ecdcb5;
 	--darkened-background: #ccc;
@@ -82,18 +217,16 @@ function addCss() {
 }
 `;
 
-	// some pages -- make just a bit darker
-	if (location.pathname.search(/^\/(help)$/) >= 0) {
-		cssText += /*css*/ `
+// some pages -- make just a bit darker
+var darkenedColor = /*css*/ `
 // general darkness
 body,#gallery-info {
 	background: var(--darkened-background);
 	color: black;
 }
 `;
-		// more important pages -- make dark
-	} else {
-		cssText += /*css*/ `
+// more important pages -- make dark
+var darkColors = /*css*/ `
 // general darkness
 body,#gallery-info,.known-information-need-edit {
 	background: var(--dark-background);
@@ -183,8 +316,16 @@ h3 {
 	color: black;
 }
 `;
-	}
 
-	GM_addStyle(cssText.replace(/\/\/.+/g, ''));
+//
+// Final CSS construction
+//
+var cssText = baseCss;
+if (location.pathname.search(/^\/(help)$/) >= 0) {
+	cssText += darkenedColor;
+} else {
+	cssText += darkColors;
 }
-addCss();
+cssText += reviewLayout;
+
+GM_addStyle(cssText.replace(/\/\/.+/g, ''));
